53cdda63d541980a6d7af48e640cb3bc5dd6e11d
solomonenanaw <sekewbeg@gmail.com>
2022-09-26 19:01:20 +0000

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Lists all the available object attributes and methods
+"""
+
+
+def lookup(obj):
+    """ Returns a list of an objects attributes and methods
+    """
+    return dir(obj)
