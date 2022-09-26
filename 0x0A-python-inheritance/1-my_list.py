73ff784252d852b1a8dd77f8af483a8259a1aa04
solomonenanaw <sekewbeg@gmail.com>
2022-09-26 19:02:57 +0000

    add

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Sorts  a list of ints in ascending order
+"""
+
+
+class MyList(list):
+      """ List manager
+      """
+      def print_sorted(self):
+          """ Sorts and prints a list of numbers in ascending order
+          """
+          print(sorted(self))
