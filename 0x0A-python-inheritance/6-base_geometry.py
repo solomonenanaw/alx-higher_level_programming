5ed5a1b55cf3051f0787c4a4efd287c4416d9d43
solomonenanaw <sekewbeg@gmail.com>
2022-09-26 19:10:37 +0000

    all

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Another template base class for geometric objects
+"""
+
+
+class BaseGeometry:
+      """ Base class or geometric objects
+      """
+      def area(self):
+          """ Calculate the area of a geometric object
+          """
+          raise Exception("area() is not implemented")
