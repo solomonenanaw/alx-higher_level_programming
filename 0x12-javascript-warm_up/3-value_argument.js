8524104bdd6e50cc7bb1f8228e5f10175ce0a2dd
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2022-11-28 21:05:44 +0300

    Update 3-value_argument.js
    add

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 if (argv[2] === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(argv[2]);
+  console.log(argv[2]);
 }
