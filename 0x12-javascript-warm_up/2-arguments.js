1c94ea57b38a3eaecfac27f243760188e5252e2d
solomonenanaw <sekewbeg@gmail.com>
2022-11-28 17:41:41 +0000

    all

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
-
+const argv = process.argv;
+if (argv.length == 1)
+{
+	console.log("Argument found")
+}
+else if (argv.length > 1)
+{
+	console.log("No argument")
+}
+else (argv.length < 1)
+{
+	console.log("No argument")
+}
