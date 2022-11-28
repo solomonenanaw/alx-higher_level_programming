8edc25a00599842a3fe9d6dcb0e292c99c148e84
solomonenanaw <sekewbeg@gmail.com>
2022-11-28 17:34:30 +0000

    fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-const a = "C is fun";
-const b = "Python is cool"
-const c = "JavaScript is amazing"
-console.log(a);
-console.log(b);
-console.log(c);
-
+const multiMsg = 'C is fun\n' + 'Python is cool\n' + 'JavaScript is amazing';
+console.log(multiMsg);
