7a6c186639e9b742223be0a264ed1f99ad52b954
solomonenanaw <sekewbeg@gmail.com>
2022-11-28 18:30:50 +0000

    fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv;
 const num = parseInt(argv[2]);
-if (isNaN(num)) {
+if (isNaN(num)){
 	console.log('Not a number');
-} else {
-	console.log(`My number: ${num}`);
+}else{
+	console.log('My number: ${num}');
 }
