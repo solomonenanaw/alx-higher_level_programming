4a029a5172e27e3e0b2527d59889a1a99abb4130
solomonenanaw <sekewbeg@gmail.com>
2022-11-29 19:53:10 +0000

    ff

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const newlist = [];
   for (let i = list.length - 1; i >= 0; i--) {
-     newlist.push(list[i]);
+    newlist.push(list[i]);
   }
   return newlist;
 };
