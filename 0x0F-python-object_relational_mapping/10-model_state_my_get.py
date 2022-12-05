dad0301423deb8cf0634e07cbe4773dc7ac992e3
solomonenanaw <sekewbeg@gmail.com>
2022-12-05 12:44:02 +0000

    fix

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ prints the State object with the name passed as argument from the
- database hbtn_0e_6_usa"""
+ database hbtn_0e_6_usa
+ """
 
  from sys import argv
  from sqlalchemy import create_engine
