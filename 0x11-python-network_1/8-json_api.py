b005eca45ddee1a8204e6aec2a904c2653c328fa
solomonenanaw <sekewbeg@gmail.com>
2022-12-16 14:10:37 +0000

    all

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' Search API '''
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    data = {'q': ''}
+
+    try:
+        data['q'] = sys.argv[1]
+    except Exception:
+        pass
+
+    res = requests.post('http://0.0.0.0:5000/search_user', data)
+
+    try:
+        jsonify = res.json()
+        if not jsonify:
+            print('No result')
+        else:
+            print('[{}] {}'.format(jsonify.get('id'), jsonify.get('name')))
+
+    except Exception:
+        print('Not a valid JSON')
