2df26b97200290cb31fd47f79665e2a3244a9c59
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2022-12-21 05:42:33 +0300

    Update 100-github_commits.py
    add

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""
-Holberton School Interview Question:
-A Module to list the last 10 commits of a
-git repo.
-Usage: >> $  ./100-github_commits.py @arg1 @arg2
-    @arg1: repository name
-    @arg2: owner name
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
 import sys
 import requests
 
 
-def apidata():
-    """pharses repo detail fromuser input and lists last 10 commits"""
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
-                                                              sys.argv[1])
-    result = requests.get(url)
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
     try:
-        r = result.json()
         for i in range(10):
-            print("{}: {}".format(r[i]["sha"],
-                                  r[i]["commit"]["author"]["name"]))
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
-
-
-if __name__ == "__main__":
-    apidata()
