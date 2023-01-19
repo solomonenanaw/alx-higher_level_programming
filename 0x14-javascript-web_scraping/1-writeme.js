74d83269b849d49e3509146a3b4e0cbe3fdb9211
Solomon Enanaw Kebede <101396246+solomonenanaw@users.noreply.github.com>
2023-01-19 20:56:21 +0300

    Update 1-writeme.js
    add

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-A JS that writes a string to a file
+// A JS that writes a string to a file
 const fs = require('fs');
 const argstr = process.argv[3];
 fs.writeFile(process.argv[2], argstr, function (err, argstr) {
