37b3b88bd7ac4835a272d7f5e3ab6bd246335d80
solomonenanaw <sekewbeg@gmail.com>
2023-01-19 17:51:42 +0000

    write

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+A JS that writes a string to a file
+const fs = require('fs');
+const argstr = process.argv[3];
+fs.writeFile(process.argv[2], argstr, function (err, argstr) {
+  if (err) {
+    console.log(err);
+  }
+});
