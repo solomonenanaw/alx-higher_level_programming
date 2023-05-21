93d96d3f04c5e9ef2b51ec4b77ff782a6021d725
solomonenanaw <sekewbeg@gmail.com>
2023-05-21 06:42:06 +0000

    update

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,1 +1,4 @@
-document.querySelector('HEADER').style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function() {
+	  var header = document.querySelector('header');
+	  header.style.color = '#FF0000';
+});
